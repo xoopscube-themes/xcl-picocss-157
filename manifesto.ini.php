1ea286f03b538de8398c7b1f2d4a9f8cf0729363
nunoluciano <nunoluciano@yahoo.com>
2023-03-18 20:52:47 +0100

    XCL Theme PicoCSS 1.5.7 update

---
+++ manifesto.ini.php
@@ -0,0 +1,22 @@
+<?php
+/**
+
+[Manifesto]
+Name="XCL PicoCSS 157"
+Depends=Legacy_RenderSystem,legacy
+ThemeOptions="index.php"
+Url="https://github.com/xoopscube-themes/xcl-picocss-157"
+Version="1.5.7"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Nuno Luciano @gigamaster
+ScreenShot="screenshot-picocss-157.jpg"
+Description="XCL PicoCSS with min layout, left sidebar and right panel. Built with Pico.css v1.5.7"
+W3C=NG
+
+License="MIT"
+
+*/
+
